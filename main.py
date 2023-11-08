56842f7d2b8c4e1ef326d4c9911e294d7c6a6809
nani-samireddy <nanisamireddy05@gmail.com>
2023-11-08 22:46:30 +0530

    added characters codes dict

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ tab = digitalio.DigitalInOut(btn1_pin)
 tab.direction = digitalio.Direction.INPUT
 tab.pull = digitalio.Pull.DOWN
 
+# braille options
+enable_numbers = False
+enable_letters = True
+enable_capital_letters = False
+
+
 # numbers braille dict contains the braille combination for numbers and the corresponding keycode
 numbers_braille_dict = {
     (False,True,False,True,True,False): Keycode.ZERO,
@@ -33,6 +39,34 @@ numbers_braille_dict = {
     (True, True, False, False, True, False): Keycode.EIGHT,
     (False, True, False, True, False, False): Keycode.NINE
     }
+letters_braille_dict = {
+    (True, False, False, False, False, False): Keycode.A,
+    (True, True, False, False, False, False): Keycode.B,
+    (True, False, False, True, False, False): Keycode.C,
+    (True, False, False, True, True, False): Keycode.D,
+    (True, False, False, False, False, True): Keycode.E,
+    (True, True, False, True, False, False): Keycode.F,
+    (True, True, False, True, True, False): Keycode.G,
+    (True, True, False, False, True, False): Keycode.H,
+    (False, False, True, True, False, False): Keycode.I,
+    (False, True, True, True, False, False): Keycode.J,
+    (True, False, True, False, False, False): Keycode.K,
+    (True, True, True, False, False, False): Keycode.L,
+    (True, False, True, True, False, False): Keycode.M,
+    (True, False, True, True, True, False): Keycode.N,
+    (True, False, True, False, True, False): Keycode.O,
+    (True, True, True, True, False, False): Keycode.P,
+    (True, True, True, True, True, False): Keycode.Q,
+    (True, True, True, False, True, False): Keycode.R,
+    (False, True, True, True, False, False): Keycode.S,
+    (False, True, True, True, True, False): Keycode.T,
+    (True, False, True, False, False, True): Keycode.U,
+    (True, True, True, False, False, True): Keycode.V,
+    (False, True, False, True, True, True): Keycode.W,
+    (True, False, True, True, False, True): Keycode.X,
+    (True, False, True, True, True, True): Keycode.Y,
+    (True, False, True, False, True, True): Keycode.Z
+}
 
 def gpio_to_braille():
     braile_input = tuple(pin.value for pin in braille_pins)
@@ -41,6 +75,7 @@ def gpio_to_braille():
         keyboard.press(numbers_braille_dict[braile_input])
         time.sleep(0.1)
         keyboard.release(numbers_braille_dict[braile_input])
+    
     
 
 # loop to check for button press
