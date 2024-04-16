86bdf2c58651d3356f01397e40e5f70642ed6eef
nani-samireddy <nanisamireddy05@gmail.com>
2024-04-16 18:07:42 +0530

    Fix modes and added missing braille codes

--- README.md
+++ README.md
@@ -3,4 +3,27 @@ A simple braille pad for the visually impaired. It uses a 3x2 grid of buttons to
 
 ## Installation
 1. Clone the repository
-2. 
+2. Open the project in [Arduino IDE](https://www.arduino.cc/en/software)
+3. Add ESP32 support to the Arduino IDE
+   1. Go to File > Preferences
+   2. Add `https://dl.espressif.com/dl/package_esp32_index.json` to the Additional Board Manager URLs
+   3. Go to Tools > Board > Boards Manager
+   4. Search for `esp32` and install the package
+   5. Select the ESP32 board from Tools > Board
+4. Import the required libraries
+   1. ESP32-BLE-Keyboard.zip (Provided in the repository folder) 
+   2. By going into Arduino> Sketch > Include Library > Add .ZIP Library
+5. Connect the ESP32 to your computer
+6. Upload the code to the ESP32 by clicking the upload button
+
+---
+### Components
+- ESP32
+- 9x Push buttons
+- Jump wires
+- USB cable
+- Vibration motor (Optional)
+
+---
+###Connection
+- Connect the push buttons to the ESP32 as shown in the diagram below
--- brailpadesp32/brailpadesp32.ino
+++ brailpadesp32/brailpadesp32.ino
@@ -1,179 +1,219 @@
-/**
- * Include the libraries
- */
 #include <BleKeyboard.h>
+#include <map>
 
-//  Bluetooth Keyboard object
 BleKeyboard bleKeyboard;
 
-/**
- * Define the pins for the buttons
- * The buttons are connected to the ESP32 pins
- * The pins are defined as INPUT_PULLUP
- * This means that the pin will read HIGH when the button is not pressed
- * and LOW when the button is pressed
- * The buttons are connected to the ESP32 pins as follows:
- * BUTTON_1_PIN 15
- * BUTTON_2_PIN 2
- * BUTTON_3_PIN 0
- * BUTTON_4_PIN 4
- * BUTTON_5_PIN 16
- * BUTTON_6_PIN 17
- * BUTTON_7_PIN 5
- * BUTTON_8_PIN 18
- */
-#define BUTTON_1_PIN 15
-#define BUTTON_2_PIN 2
-#define BUTTON_3_PIN 0
-#define BUTTON_4_PIN 4
-#define BUTTON_5_PIN 16
-#define BUTTON_6_PIN 17
-#define BUTTON_7_PIN 5
-#define BUTTON_8_PIN 18
+#define BRAILLE_INPUT_1 15
+#define BRAILLE_INPUT_2 2
+#define BRAILLE_INPUT_3 0
+#define BRAILLE_INPUT_4 12
+#define BRAILLE_INPUT_5 14
+#define BRAILLE_INPUT_6 27
+#define CONTROL_BUTTON_1 4
+#define CONTROL_BUTTON_2 26
+#define CONTROL_BUTTON_3 16
 
+// Vibration motor pin
+#define VBP 22
 
-/**
- * @brief  - Map of the numbers to the braille representation
- * - The map is a dictionary with the key as the braille representation
- * - and the value as the number
- */
+enum class Mode
+{
+  ToNumber,
+  ToCharacter,
+  ToCapital
+};
+
+Mode current_mode = Mode::ToCharacter;
+
+std::map<std::string, Mode> modes_map = {
+    {"001111", Mode::ToNumber},
+    {"000011", Mode::ToCharacter},
+    {"000001", Mode::ToCapital},
+};
+
 std::map<std::string, std::string> numbers_map = {
-  "100000": "1",
-  "110000": "2",
-  "100100": "3",
-  "100110": "4",
-  "100010": "5",
-  "110100": "6",
-  "110110": "7",
-  "110010": "8",
-  "010100": "9",
-  "010110": "0"
+    {"100000", "1"},
+    {"110000", "2"},
+    {"100100", "3"},
+    {"100110", "4"},
+    {"100010", "5"},
+    {"110100", "6"},
+    {"110110", "7"},
+    {"110010", "8"},
+    {"010100", "9"},
+    {"010110", "0"},
 };
 
-/**
- * @brief  - Map of the alphabets to the braille representation
- * - The map is a dictionary with the key as the braille representation
- * - and the value as the alphabet
- */
 std::map<std::string, std::string> alphabets_map = {
-  "100000": "a",
-  "110000": "b",
-  "100100": "c",
-  "100110": "d",
-  "100010": "e",
-  "110100": "f",
-  "110110": "g",
-  "110010": "h",
-  "010100": "i",
-  "010110": "j",
-  "101000": "k",
-  "111000": "l",
-  "101100": "m",
-  "101110": "n",
-  "101010": "o",
-  "111100": "p",
-  "111110": "q",
-  "111010": "r",
-  "011100": "s",
-  "011110": "t",
-  "101001": "u",
-  "111001": "v",
-  "010111": "w",
-  "101101": "x",
-  "101111": "y",
-  "101011": "z"
+    {"100000", "a"},
+    {"110000", "b"},
+    {"100100", "c"},
+    {"100110", "d"},
+    {"100010", "e"},
+    {"110100", "f"},
+    {"110110", "g"},
+    {"110010", "h"},
+    {"010100", "i"},
+    {"010110", "j"},
+    {"101000", "k"},
+    {"111000", "l"},
+    {"101100", "m"},
+    {"101110", "n"},
+    {"101010", "o"},
+    {"111100", "p"},
+    {"111110", "q"},
+    {"111010", "r"},
+    {"011100", "s"},
+    {"011110", "t"},
+    {"101001", "u"},
+    {"111001", "v"},
+    {"010111", "w"},
+    {"101101", "x"},
+    {"101111", "y"},
+    {"101011", "z"},
 };
 
-/**
- * @brief  - Map of the special characters to the braille representation
- * - The map is a dictionary with the key as the braille representation
- * - and the value as the special character
- */
-
-/**
- * @brief  - function sets the input pins to INPUT_PULLUP
- * - This means that the pin will read HIGH when the button is not pressed
- * - and LOW when the button is pressed
- * @return - void
- */
-void set_input_pins_pullup(){
-  pinMode(BUTTON_1_PIN, INPUT_PULLUP); 
-  pinMode(BUTTON_2_PIN, INPUT_PULLUP); 
-  pinMode(BUTTON_3_PIN, INPUT_PULLUP); 
-  pinMode(BUTTON_4_PIN, INPUT_PULLUP); 
-  pinMode(BUTTON_5_PIN, INPUT_PULLUP); 
-  pinMode(BUTTON_6_PIN, INPUT_PULLUP); 
-  pinMode(BUTTON_7_PIN, INPUT_PULLUP); 
-  pinMode(BUTTON_8_PIN, INPUT_PULLUP); 
+void set_input_pins_pullup()
+{
+  pinMode(BRAILLE_INPUT_1, INPUT_PULLUP);
+  pinMode(BRAILLE_INPUT_2, INPUT_PULLUP);
+  pinMode(BRAILLE_INPUT_3, INPUT_PULLUP);
+  pinMode(BRAILLE_INPUT_4, INPUT_PULLUP);
+  pinMode(BRAILLE_INPUT_5, INPUT_PULLUP);
+  pinMode(BRAILLE_INPUT_6, INPUT_PULLUP);
+  pinMode(CONTROL_BUTTON_1, INPUT_PULLUP);
+  pinMode(CONTROL_BUTTON_2, INPUT_PULLUP);
+  pinMode(VBP, OUTPUT);
 }
 
-
-/**
- * @brief - function triggers haptic feedback
- * @return - void
- */
-void trigger_haptic_feedback(){
-  // Add code to trigger haptic feedback
+void send_haptic_feedback()
+{
+  Serial.println("Sending Haptic Feedback");
+  digitalWrite(VBP, HIGH);
+  delay(800);
+  digitalWrite(VBP, LOW);
 }
 
-/**
- * @brief - setup function runs once when you press reset or power the board
- * @return - void 
- */
-void setup() {
+std::string decode_braille_input()
+{
+  std::string braille_input = "";
+  braille_input += digitalRead(BRAILLE_INPUT_1) == LOW ? "1" : "0";
+  braille_input += digitalRead(BRAILLE_INPUT_2) == LOW ? "1" : "0";
+  braille_input += digitalRead(BRAILLE_INPUT_3) == LOW ? "1" : "0";
+  braille_input += digitalRead(BRAILLE_INPUT_4) == LOW ? "1" : "0";
+  braille_input += digitalRead(BRAILLE_INPUT_5) == LOW ? "1" : "0";
+  braille_input += digitalRead(BRAILLE_INPUT_6) == LOW ? "1" : "0";
+  return braille_input;
+}
+
+// Change the mode based on the input
+void change_mode(std::string braille_input)
+{
+  if (braille_input != "000000" && modes_map.find(braille_input) != modes_map.end())
+  {
+    Serial.println("Changing mode");
+    // if it is to_character mode or to_capital mode, then toggle the mode
+    if ((current_mode == Mode::ToCharacter || current_mode == Mode::ToCapital) && (modes_map[braille_input] == Mode::ToCharacter || modes_map[braille_input] == Mode::ToCapital))
+    {
+      current_mode = current_mode == Mode::ToCharacter ? Mode::ToCapital : Mode::ToCharacter;
+    }
+    else
+    {
+      current_mode = modes_map[braille_input];
+    }
+  }
+}
+
+void send_braille_input()
+{
+  std::string braille_input = decode_braille_input();
+
+  // Log the braille input and current mode
+  Serial.println("Braille Input");
+  Serial.println(braille_input.c_str());
+
+  // Check if the input is a mode change
+  if (modes_map.find(braille_input) != modes_map.end())
+  {
+    change_mode(braille_input);
+    send_haptic_feedback();
+    return;
+  }
+
+  switch (current_mode)
+  {
+  case Mode::ToNumber:
+    if (numbers_map.find(braille_input) != numbers_map.end())
+    {
+      const char *number_str = numbers_map[braille_input].c_str();
+      size_t len = strlen(number_str);
+      for (size_t i = 0; i < len; i++)
+      {
+        bleKeyboard.write(static_cast<uint8_t>(number_str[i]));
+      }
+    }
+    return;
+    break;
+  case Mode::ToCharacter:
+    if (alphabets_map.find(braille_input) != alphabets_map.end())
+    {
+      const char *char_str = alphabets_map[braille_input].c_str();
+      size_t len = strlen(char_str);
+      for (size_t i = 0; i < len; i++)
+      {
+        bleKeyboard.write(static_cast<uint8_t>(char_str[i]));
+      }
+    }
+    return;
+    break;
+  case Mode::ToCapital:
+    if (alphabets_map.find(braille_input) != alphabets_map.end())
+    {
+      const char *capital_str = alphabets_map[braille_input].c_str();
+      size_t len = strlen(capital_str);
+      for (size_t i = 0; i < len; i++)
+      {
+        bleKeyboard.write(static_cast<uint8_t>(toupper(capital_str[i])));
+      }
+    }
+    return;
+    break;
+  default:
+    send_haptic_feedback();
+    break;
+  }
+  send_haptic_feedback();
+}
+
+void disconnectBluetooth()
+{
+  if (bleKeyboard.isConnected())
+  {
+    bleKeyboard.end();
+  }
+}
+
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
   set_input_pins_pullup();
   bleKeyboard.begin();
 }
 
+void loop()
+{
+  if (bleKeyboard.isConnected())
+  {
+    Serial.println("Bluetooth connected");
+    send_braille_input();
+    // send_haptic_feedback();
+  }
+  else
+  {
+    Serial.println("Bluetooth Not connected");
+    Serial.println("Waiting 1 seconds...");
+  }
 
-/**
- * @brief loop function runs over and over again forever
- * 
- * @return * void 
- */
-void loop() {
-  if(bleKeyboard.isConnected()) {
-      Serial.println("Bluetooth connected");
-    if(digitalRead(BUTTON_1_PIN) == LOW){
-      Serial.println("BUTTON 1 PRESSED");
-      bleKeyboard.write('a');
-    }
-    if(digitalRead(BUTTON_2_PIN) == LOW){
-      Serial.println("BUTTON 2 PRESSED");
-      bleKeyboard.write('b');
-    }
-    if(digitalRead(BUTTON_3_PIN) == LOW){
-      Serial.println("BUTTON 3 PRESSED");
-      bleKeyboard.write('c');
-    }
-    if(digitalRead(BUTTON_4_PIN) == LOW){
-      Serial.println("BUTTON 4 PRESSED");
-      bleKeyboard.write('d');
-    }
-    if(digitalRead(BUTTON_5_PIN) == LOW){
-      Serial.println("BUTTON 5 PRESSED");
-      bleKeyboard.write('e');
-    }
-    if(digitalRead(BUTTON_6_PIN) == LOW){
-      Serial.println("BUTTON 6 PRESSED");
-      bleKeyboard.write('f');
-    }
-    if(digitalRead(BUTTON_7_PIN) == LOW){
-      Serial.println("BUTTON 7 PRESSED");
-      bleKeyboard.write('g');
-    }
-    if(digitalRead(BUTTON_8_PIN) == LOW){
-      Serial.println("BUTTON 8 PRESSED");
-      bleKeyboard.write('h');
-    }
-    
-  }else{
-      Serial.println("Bluetooth Not connected");
-      Serial.println("Waiting 1 seconds...");
-    }
-  
-  delay(1000);
+  delay(600);
 }
-
