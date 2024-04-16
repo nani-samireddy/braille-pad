96aa72e58382200c2ae090fc113146e8e6c51448
nani-samireddy <nanisamireddy05@gmail.com>
2024-04-16 18:31:09 +0530

    Add alphabets and numbers with capitals support

--- brailpadesp32/brailpadesp32.ino
+++ brailpadesp32/brailpadesp32.ino
@@ -16,6 +16,7 @@ BleKeyboard bleKeyboard;
 // Vibration motor pin
 #define VBP 22
 
+// Enum to represent different modes
 enum class Mode
 {
   ToNumber,
@@ -23,14 +24,17 @@ enum class Mode
   ToCapital
 };
 
+// Initialize current mode to ToCharacter
 Mode current_mode = Mode::ToCharacter;
 
+// Map to store the conversion between braille patterns and modes
 std::map<std::string, Mode> modes_map = {
     {"001111", Mode::ToNumber},
     {"000011", Mode::ToCharacter},
     {"000001", Mode::ToCapital},
 };
 
+// Map to store the conversion between braille patterns and numbers
 std::map<std::string, std::string> numbers_map = {
     {"100000", "1"},
     {"110000", "2"},
@@ -44,6 +48,7 @@ std::map<std::string, std::string> numbers_map = {
     {"010110", "0"},
 };
 
+// Map to store the conversion between braille patterns and alphabets
 std::map<std::string, std::string> alphabets_map = {
     {"100000", "a"},
     {"110000", "b"},
@@ -73,6 +78,14 @@ std::map<std::string, std::string> alphabets_map = {
     {"101011", "z"},
 };
 
+/**
+ * @brief Set the input pins to pull-up mode and the vibration motor pin to output.
+ *
+ * This function sets the input pins BRAILLE_INPUT_1 to BRAILLE_INPUT_6 and CONTROL_BUTTON_1 to CONTROL_BUTTON_2
+ * to INPUT_PULLUP mode, and sets the VBP pin to OUTPUT mode for controlling the vibration motor.
+ *
+ * @return void
+ */
 void set_input_pins_pullup()
 {
   pinMode(BRAILLE_INPUT_1, INPUT_PULLUP);
@@ -86,14 +99,30 @@ void set_input_pins_pullup()
   pinMode(VBP, OUTPUT);
 }
 
+/**
+ * @brief Send haptic feedback by toggling the vibration motor pin.
+ *
+ * This function toggles the VBP pin HIGH for a brief duration to simulate haptic feedback,
+ * then brings it LOW again.
+ *
+ * @return void
+ */
 void send_haptic_feedback()
 {
   Serial.println("Sending Haptic Feedback");
   digitalWrite(VBP, HIGH);
-  delay(800);
+  delay(200);
   digitalWrite(VBP, LOW);
 }
 
+/**
+ * @brief Decode the braille input by reading the digital states of input pins.
+ *
+ * This function reads the digital states of the input pins BRAILLE_INPUT_1 to BRAILLE_INPUT_6
+ * and constructs a binary string representing the braille input.
+ *
+ * @return std::string - The braille input as a binary string.
+ */
 std::string decode_braille_input()
 {
   std::string braille_input = "";
@@ -106,13 +135,21 @@ std::string decode_braille_input()
   return braille_input;
 }
 
-// Change the mode based on the input
+/**
+ * @brief Change the current mode based on the input braille pattern.
+ *
+ * This function checks the input braille pattern and changes the current mode accordingly.
+ * If the pattern corresponds to a mode switch, it toggles between ToCharacter and ToCapital modes.
+ *
+ * @param braille_input - The input braille pattern to analyze for mode change.
+ * @return void
+ */
 void change_mode(std::string braille_input)
 {
   if (braille_input != "000000" && modes_map.find(braille_input) != modes_map.end())
   {
     Serial.println("Changing mode");
-    // if it is to_character mode or to_capital mode, then toggle the mode
+    // Toggle the mode between ToCharacter and ToCapital
     if ((current_mode == Mode::ToCharacter || current_mode == Mode::ToCapital) && (modes_map[braille_input] == Mode::ToCharacter || modes_map[braille_input] == Mode::ToCapital))
     {
       current_mode = current_mode == Mode::ToCharacter ? Mode::ToCapital : Mode::ToCharacter;
@@ -124,10 +161,21 @@ void change_mode(std::string braille_input)
   }
 }
 
+/**
+ * @brief Send braille input as keyboard output based on the current mode.
+ *
+ * This function processes the braille input based on the current mode and sends the corresponding
+ * keyboard output using the BLE keyboard.
+ *
+ * @return void
+ */
 void send_braille_input()
 {
   std::string braille_input = decode_braille_input();
-
+  if (braille_input == "000000")
+  {
+    return;
+  }
   // Log the braille input and current mode
   Serial.println("Braille Input");
   Serial.println(braille_input.c_str());
@@ -140,6 +188,7 @@ void send_braille_input()
     return;
   }
 
+  // Switch statement based on the current mode
   switch (current_mode)
   {
   case Mode::ToNumber:
@@ -151,9 +200,11 @@ void send_braille_input()
       {
         bleKeyboard.write(static_cast<uint8_t>(number_str[i]));
       }
+      return;
     }
-    return;
+    send_haptic_feedback();
     break;
+
   case Mode::ToCharacter:
     if (alphabets_map.find(braille_input) != alphabets_map.end())
     {
@@ -163,9 +214,11 @@ void send_braille_input()
       {
         bleKeyboard.write(static_cast<uint8_t>(char_str[i]));
       }
+      return;
     }
-    return;
+    send_haptic_feedback();
     break;
+
   case Mode::ToCapital:
     if (alphabets_map.find(braille_input) != alphabets_map.end())
     {
@@ -175,16 +228,24 @@ void send_braille_input()
       {
         bleKeyboard.write(static_cast<uint8_t>(toupper(capital_str[i])));
       }
+      return;
     }
-    return;
+    send_haptic_feedback();
     break;
+
   default:
     send_haptic_feedback();
     break;
   }
-  send_haptic_feedback();
 }
 
+/**
+ * @brief Disconnect Bluetooth if connected.
+ *
+ * This function checks if the BLE keyboard is connected and disconnects it if so.
+ *
+ * @return void
+ */
 void disconnectBluetooth()
 {
   if (bleKeyboard.isConnected())
@@ -193,6 +254,14 @@ void disconnectBluetooth()
   }
 }
 
+/**
+ * @brief Setup function to initialize serial communication, pins, and BLE keyboard.
+ *
+ * This function is called once during startup to initialize serial communication,
+ * set input pins to pull-up mode, and start the BLE keyboard service.
+ *
+ * @return void
+ */
 void setup()
 {
   Serial.begin(115200);
@@ -201,13 +270,21 @@ void setup()
   bleKeyboard.begin();
 }
 
+/**
+ * @brief Loop function to check for Bluetooth connection and send braille input.
+ *
+ * This function is called repeatedly in a loop. It checks if the BLE keyboard is connected,
+ * and if so, it sends the braille input based on the current mode.
+ * If the keyboard is not connected, it waits for 600 milliseconds before checking again.
+ *
+ * @return void
+ */
 void loop()
 {
   if (bleKeyboard.isConnected())
   {
     Serial.println("Bluetooth connected");
     send_braille_input();
-    // send_haptic_feedback();
   }
   else
   {
